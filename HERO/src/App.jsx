67b7f2c345e06d63c68d4ba55eae4c85eb0e68f7
Ramiro Kuspita <rastictac>
2023-10-18 19:19:29 -0300

    loading

---
+++ HERO/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { Loading } from './Components/loading'
+
+
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        {/* <Loading></Loading> */}
+        
+    </>
+  )
+}
+
+export default App
