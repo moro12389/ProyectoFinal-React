0752fca9e530a41617bb730f7bde92c28ab0850d
polsze <polbarrios835237@gmail.com>
2023-11-06 20:28:57 -0300

    2nd commit

--- nav/src/components/NavBar.jsx
+++ nav/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Logo from '../../public/img/logo.png';
 import chf from '../../public/img/chff1.png';
 import { Fragment } from 'react'
@@ -9,7 +9,7 @@ import {
   Link,
 } from "react-router-dom";
 import { motion } from 'framer-motion'
-
+import BagDropdown from './bagDropdown';
 
 
 
@@ -18,6 +18,23 @@ function classNames(...classes) {
 }
 
 const NavBar = () => {
+  const [carrito, setCarrito] = useState([]);
+
+  useEffect(() => {
+    fetch('Json/Data.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('no se conecto');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setCarrito(data['carrito']);
+      })
+
+      .catch(error => console.error('Error no se pudo obtener:', error));
+  }, []);
+
 
   const [dropdownClick, setdropdownClick] = useState(false);
 
@@ -33,12 +50,16 @@ const NavBar = () => {
   const handleClickMenu = () => {
     setIsOpen(!isOpen)
   }
+
+  const [isBagDropdownVisible, setIsBagDropdownVisible] = useState(false);
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+const [isNavbarVisible, setIsNavbarVisible] = useState(true);
+
   return (
     <>
       <header className="">
-
-
-        <div className='hidden ml-1 sm:flex'>
+        <div className='hidden pl-4 sm:flex xs:pl-1'>
           <div className='flex flex-col relative top-7'>
             <button className='flex-col justify-center items-center lg:flex' onClick={handleClickMenu}>
               <span className={`bg-black  block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
@@ -56,8 +77,8 @@ const NavBar = () => {
                 type="search" name="search" placeholder="Search" />
               <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
                 <svg className="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                   version="1.1" id="Capa_1" x="0px" y="0px"
-                  viewBox="0 0 56.966 56.966" 
+                  version="1.1" id="Capa_1" x="0px" y="0px"
+                  viewBox="0 0 56.966 56.966"
                   width="512px" height="512px">
                   <path
                     d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
@@ -66,16 +87,16 @@ const NavBar = () => {
             </div>
             <h3>Menu</h3>
             <div className='grid grid-cols-2'>
-            <div className='flex flex-col m-auto'>
-            {textDropdown.map((text, index) => (
-              <li className='' key={index}>{text}</li>
-            ))}
+              <div className='flex flex-col m-auto'>
+                {textDropdown.map((text, index) => (
+                  <li className='' key={index}>{text}</li>
+                ))}
+              </div>
+              <div>
+                <img src={chf} alt="" className="" />
+              </div>
             </div>
-            <div>
-              <img src={chf} alt="" className="" />
-            </div>
-            </div>
-            <div>
+            <div className=''>
               <a href="" className='px-4 py-2 h-6 rounded-2xl bg-gray-200 mr-2'>About Us</a>
               <a href="" className='px-4 py-2 h-6 rounded-2xl bg-gray-200'>Contacts</a>
             </div>
@@ -85,15 +106,14 @@ const NavBar = () => {
 
 
 
-
-        <div className='flex flex-row items-center justify-around'>
+        <div className='flex flex-row items-center justify-around sm:justify-around'>
           <div className=''>
-            <img src={Logo} alt="" className="sm:w-16 sm:ml-2" />
+            <img src={Logo} alt="" className="sm:w-24 sm:ml-12 xs:w-16 xs:ml-9" />
           </div>
           <ul className='flex flex-row '>
             <Menu as="div" className="relative inline-block text-left sm:hidden">
               <div>
-                <Menu.Button className="inline-flex w-full justify-center gap-x-1.5  bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm " onClick={handleClick}>
+                <Menu.Button className="inline-flex w-full justify-center gap-x-1.5  bg-white px-3 py-2 text-sm font-semibold text-gray-900 " onClick={handleClick}>
                   {dropdownClick ? "New" : "Menu"}
                   {dropdownClick ? (<ChevronUpIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />) : (<ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />)}
                 </Menu.Button>
@@ -108,7 +128,7 @@ const NavBar = () => {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
               >
-                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ">
+                <Menu.Items className="absolute right-0 z-10 w-56 origin-top-right rounded-md bg-white ">
                   <div className="py-1 ">
                     {textDropdown.map((text, index) => (
                       <Menu.Item key={index}>
@@ -118,7 +138,7 @@ const NavBar = () => {
                             href="#"
                             className={classNames(
                               active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
+                              'block px-4 py-2 text-sm text-right'
                             )}
                           >
                             {text}
@@ -130,20 +150,28 @@ const NavBar = () => {
                 </Menu.Items>
               </Transition>
             </Menu>
-            <li className='mx-16 py-2 text-sm font-semibold sm:mx-0 sm:mr-4 sm:whitespace-nowrap'>About Us</li>
-            <li className='py-2 text-sm font-semibold'>Contacts</li>
+            <li className='mx-16 py-2 text-sm font-semibold sm:mx-0 sm:mr-4 sm:whitespace-nowrap xl:block  sm:hidden'>About Us</li>
+            <li className='py-2 text-sm font-semibold xl:block sm:hidden'>Contacts</li>
           </ul>
           <div className='flex flex-row py-2'>
-            <Icon icon="devicon:facebook" className='mr-4 sm:mr-1' width="30" />
+            <Icon icon="devicon:facebook" className='mr-4 xs:mr-1' width="30" />
             <Icon icon="skill-icons:instagram" width="30" />
           </div>
+
           <div className='flex flex-row items-center py-2'>
-  <Link to="/login" className='mr-4 border-2 border-gray-600 px-6 py-1 rounded-2xl sm:px-2 sm:mr-0 sm:ml-1'>Login</Link>
-  <div className="relative">
-    <Icon icon="uil:shopping-bag" width="40" />
-    <div className="absolute top-3 right-2 w-6 h-6 rounded-full  flex items-center justify-center text-black text-xs font-bold">5</div>
-  </div>
-</div>
+            <Link to="/login" className='mr-4 border-2 border-gray-600 px-6 py-1 rounded-2xl sm:px-2 sm:mr-2 sm:ml-3 xs:ml-1 xs:mr-1'>Login</Link>
+
+            <div>
+              <div className="relative" onClick={() => setIsBagDropdownVisible(!isBagDropdownVisible)}>
+                <Icon icon="uil:shopping-bag" width="40" />
+                <div className="absolute top-3 right-2 w-6 h-6 rounded-full flex items-center justify-center text-black text-xs font-bold cursor-pointer">
+                  {carrito.length}
+                </div>
+              </div>
+
+              {isBagDropdownVisible && <BagDropdown />}
+            </div>
+          </div>
         </div>
       </header>
     </>
